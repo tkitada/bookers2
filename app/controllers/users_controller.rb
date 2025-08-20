c19636c8b7f461fa3588144057aca5541d29f697
Ubuntu <ubuntu@ip-172-31-13-65.ap-northeast-1.compute.internal>
2025-08-20 15:42:34 +0000

    Initial Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @user = current_user
+  end
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @new_book = Book.new
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction)
+  end
+end
